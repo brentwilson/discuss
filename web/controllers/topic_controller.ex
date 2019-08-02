d23365718facfab1f0ecead1c620af662cc8d598
Brent Wilson <brent@prmconnect.com>
2019-08-02 09:39:20 -0700

    prefix params on new topic route

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,7 +2,7 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
